6e151f18f7d1a4167e600dd0fece733d158a3989
Azra Javed <azraj063@gmail.com>
2026-06-26 00:22:08 +0500

    Add message to check CI/CD actions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to Mern CI/CD pipeline</h1>
         <p>This is a simple app page built with TypeScript and React.</p>
+        <p> I edit this file now to check the CI/CD actions</p>
         <button onClick={() => alert('Hello from your app!')}>
           Click me
         </button>
